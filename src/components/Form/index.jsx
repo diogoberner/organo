277bfc30d7b9905de42dc381fd647989a253c335
Diogo CS Berner <diogoberner@gmail.com>
2025-04-03 09:38:54 -0300

    form created

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,24 @@
+import FormInput from "../FormInput";
+import "./Form.css";
+import teamList from "../teamList.js";
+import FormButton from "../FormButton/index.jsx";
+
+const Form = () => {
+  return (
+    <form className="form">
+      <h2>Preencha os dados para criar o card do colaborador.</h2>
+      <FormInput label="Nome" placeholder="Digite seu nome" />
+      <FormInput label="Cargo" placeholder="Digite seu cargo" />
+      <FormInput label="Imagem" placeholder="Informe o endereço da imagem" />
+      <FormInput
+        label="Time"
+        placeholder=""
+        type="select"
+        teamList={teamList}
+      />
+      <FormButton />
+    </form>
+  );
+};
+
+export default Form;
