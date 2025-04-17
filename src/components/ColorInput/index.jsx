361c8c7fd489622faffa7ff1824f23089774f5b1
Diogo CS Berner <diogoberner@gmail.com>
2025-04-16 21:57:20 -0300

    using hex to rgba npm to change secondary color

--- src/components/ColorInput/index.jsx
+++ src/components/ColorInput/index.jsx
@@ -1,40 +1,15 @@
 import "./ColorInput.css";
 import { useCallback, useContext } from "react";
 import { CategoryContext } from "../../context/CategoryContext";
+import hexToRgba from "hex-to-rgba";
 
 const ColorInput = ({ category }) => {
   const { setCategories } = useContext(CategoryContext);
 
-  function lightenColor(hex, amount = 40) {
-    let color = hex.replace("#", "");
-
-    if (color.length === 3) {
-      color = color
-        .split("")
-        .map((c) => c + c)
-        .join("");
-    }
-
-    const num = parseInt(color, 16);
-
-    let r = (num >> 16) + amount;
-    let g = ((num >> 8) & 0x00ff) + amount;
-    let b = (num & 0x0000ff) + amount;
-
-    r = Math.min(255, r);
-    g = Math.min(255, g);
-    b = Math.min(255, b);
-
-    return `#${((1 << 24) + (r << 16) + (g << 8) + b)
-      .toString(16)
-      .slice(1)
-      .toUpperCase()}`;
-  }
-
   const handleColorChange = useCallback(
     (event) => {
       const newPrimary = event.target.value;
-      const newSecondary = lightenColor(newPrimary, 140);
+      const newSecondary = hexToRgba(newPrimary, 0.4);
       setCategories((prevCategories) =>
         prevCategories.map((cat) =>
           cat.category === category.category
